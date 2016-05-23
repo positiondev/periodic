0854e40fa489a96f98e1263b5aeb473ff3060d7f
Daniel Patterson <dbp@dbpmail.net>
2016-05-22 22:20:08 -0400

    move logic into redis

--- src/System/Periodic.hs
+++ src/System/Periodic.hs
@@ -36,65 +36,55 @@ checkPeriod = 100 * seconds
 lockTimeout = 10 * hours
 
 run :: Scheduler -> IO ()
-run (Scheduler nm rconn mv) = forever $ do tasks <- readMVar mv
-                                           mapM_ (tryRunTask nm rconn) tasks
+run (Scheduler nm rconn mv) = forever $ do now <- getCurrentTime
+                                           tasks <- readMVar mv
+                                           mapM_ (tryRunTask nm rconn now) tasks
                                            threadDelay checkPeriod
 
-needsRun :: Period -> UTCTime -> Maybe UTCTime -> Maybe UTCTime -> Bool
-needsRun (Daily (Time t)) now Nothing Nothing
-  | abs (utctDayTime now - t) < fromIntegral checkPeriod * 10
-  = True
-needsRun (Daily (Time t)) now (Just last) Nothing
-  | abs (utctDayTime now - t) < fromIntegral checkPeriod * 10 &&
-    abs (diffUTCTime now last) > fromIntegral lockTimeout
-  = True
-needsRun (Daily (Time t)) now _ (Just locked)
-  | abs (diffUTCTime now locked) > fromIntegral lockTimeout
-  = True
-needsRun _ _ _ _
-  = False
-
-nextRunTime :: Period -> UTCTime -> Maybe UTCTime -> UTCTime
-nextRunTime (Daily (Time t)) now Nothing = now { utctDayTime = t }
-nextRunTime (Daily (Time t)) now (Just last) =
-  let next = last {utctDay = addDays 1 (utctDay last)}
-      adjusted = next { utctDayTime = t}
-  -- NOTE(dbp 2016-05-22):
-  in if abs (diffUTCTime next adjusted) < 1 * fromIntegral hours
-        then adjusted
-        else next
+nextRunTime :: Period -> UTCTime -> UTCTime
+nextRunTime (Daily (Time t)) now = now { utctDayTime = t }
 
 collapseError :: Either a (Maybe b) -> Maybe b
 collapseError (Left _) = Nothing
 collapseError (Right v) = v
 
+collapseNumberBoolFalse :: Either a (Maybe Integer) -> Bool
+collapseNumberBoolFalse (Left _) = False
+collapseNumberBoolFalse (Right (Just 1)) = True
+collapseNumberBoolFalse (Right (Just 0)) = False
+
 parseUnixTime = posixSecondsToUTCTime . fromIntegral . read . T.unpack . T.decodeUtf8
 
+renderUnixTime = T.encodeUtf8 . T.pack . show . utcTimeToPOSIXSeconds
 
-tryRunTask :: Text -> R.Connection -> (Text, Period, IO ()) -> IO ()
-tryRunTask pname rconn (name, period, task) =
+tryRunTask :: Text -> R.Connection -> UTCTime -> (Text, Period, IO ()) -> IO ()
+tryRunTask pname rconn now (name, period, task) =
   do let key = T.encodeUtf8 $ pname <> "-" <> name <> "-last-run"
      let locked = T.encodeUtf8 $ pname <> "-" <> name <> "-running-at"
-     now <- getCurrentTime
-     lastrun <- fmap parseUnixTime . collapseError <$> R.runRedis rconn (R.get key)
-     let nextRun = nextRunTime period now lastrun
-     -- NOTE(dbp 2016-05-22):
-     -- 1. compute when it should next run
-     -- 2. atomically lock it if need to run and not locked.
-     (k,l) <- R.runRedis rconn $ do kr <- R.get key
-                                    lr <- R.get locked
-                                    case (kr,lr) of
-                                      (Left _, Left _) -> return (Nothing, Nothing)
-                                      (Right r, Left _) -> return (r, Nothing)
-                                      (Left _, Right r) -> return (Nothing, r)
-                                      (Right r1, Right r2) -> return (r1, r2)
-     now <- getCurrentTime
-     when (needsRun period now
-                           ((posixSecondsToUTCTime . fromIntegral . read . T.unpack . T.decodeUtf8) <$> k)
-                           ((posixSecondsToUTCTime . fromIntegral . read . T.unpack . T.decodeUtf8) <$> l)) $
-       do let tstr = T.encodeUtf8 $ T.pack $ show $ utcTimeToPOSIXSeconds now
-          R.runRedis rconn $ R.set locked tstr
-          task
-          R.runRedis rconn $ do R.set key tstr
-                                R.del [locked]
-          return ()
+     let nextRun = nextRunTime period now
+     shouldRun <- collapseNumberBoolFalse <$> R.runRedis rconn
+       -- NOTE(dbp 2016-05-22): This is the heart of this whole
+       -- library. We check if should run, which involves checking the
+       -- last time that it has run (and if it is close to when the
+       -- next scheduled run is) and simultaneously if it has been
+       -- locked. Note that since we need to handle the fact that a
+       -- worker could have claimed it and then died, we have to check
+       -- even when we are not anywhere near the actual scheduled
+       -- time.
+       (R.eval "local last = redis.call('get', KEYS[1])\n\
+               \local lock = redis.call('get', KEYS[2])\n\
+               \if lock ~= nil and math.abs(ARGV[1] - lock) > ARGV[4] then\n\
+               \  redis.call('set', KEYS[1], ARGV[1])\n\
+               \  redis.call('set', KEYS[2], ARGV[1])\n\
+               \  return 1\n\
+               \elseif math.abs(ARGV[1] - ARGV[2]) < ARGV[3] and (last == nil or math.abs(last - ARGV[1]) > ARGV[4]) and lock == nil then\n\
+               \  redis.call('set', KEYS[1], ARGV[1])\n\
+               \  redis.call('set', KEYS[2], ARGV[1])\n\
+               \  redis.call(''\n\
+               \  return 1\n\
+               \else\n\
+               \  return 0\
+               \end" [key, locked] [renderUnixTime now, renderUnixTime nextRun, T.encodeUtf8 . T.pack . show $ 10 * checkPeriod, T.encodeUtf8 . T.pack . show $ lockTimeout])
+     when shouldRun $ do task
+                         R.runRedis rconn $ R.del [locked]
+                         return ()
