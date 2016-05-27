f5c6adb140c5548cc4f8fbf768eeb4b657d38543
Daniel Patterson <dbp@dbpmail.net>
2016-05-26 21:54:09 -0400

    Switch to storing time that jobs started.
    This works much better, and it made it possible to easily get the tests
    passing.

--- src/System/Periodic.hs
+++ src/System/Periodic.hs
@@ -15,7 +15,6 @@ import           Data.Time.Clock
 import           Data.Time.Clock.POSIX
 import qualified Database.Redis          as R
 
-data Scheduler = Scheduler Text R.Connection (MVar [(Text, Period, IO ())]) Int
 
 newtype Time = Time DiffTime -- time from midnight UTC
 
@@ -23,44 +22,49 @@ newtype Seconds = Seconds Int
 
 data Period = Daily Time | Every Seconds
 
-create :: Text -> R.Connection -> Int -> IO Scheduler
-create name rconn check = do mv <- newMVar []
-                             return $ Scheduler name rconn mv check
+newtype Name = Name Text
+newtype CheckInterval = CheckInterval Seconds
+newtype LockTimeout = LockTimeout Seconds
+
+data Scheduler = Scheduler { schedulerName          :: Name
+                           , schedulerRedisConn     :: R.Connection
+                           , schedulerTasks         :: MVar [(Text, Period, IO ())]
+                           , schedulerCheckInterval :: CheckInterval
+                           , schedulerLockTimout    :: LockTimeout
+                           }
+
+create :: Name -> R.Connection -> CheckInterval -> LockTimeout -> IO Scheduler
+create name rconn check lock = do mv <- newMVar []
+                                  return $ Scheduler name rconn mv check lock
 
 addTask :: Scheduler -> Text -> Period -> IO () -> IO ()
-addTask (Scheduler _ _ mv _) job when act =
-  do modifyMVar_ mv (return . (:) (job, when, act))
+addTask scheduler job when act =
+  do modifyMVar_ (schedulerTasks scheduler) (return . (:) (job, when, act))
      return ()
 
 lockTimeout = 10 * 3600
 
 run :: Scheduler -> IO ()
-run (Scheduler nm rconn mv check) = forever $
+run (Scheduler (Name nm) rconn mv (CheckInterval (Seconds check)) _) = forever $
     do now <- getCurrentTime
        tasks <- readMVar mv
        mapM_ (tryRunTask check nm rconn now) tasks
        threadDelay (check * 1000000)
 
-lastRunKey pname name =
-  T.encodeUtf8 $ pname <> "-" <> name <> "-last-run"
+lastStartedKey pname name =
+  T.encodeUtf8 $ pname <> "-" <> name <> "-last-started"
 lockedKey pname name =
   T.encodeUtf8 $ pname <> "-" <> name <> "-running-at"
 
 destroy :: Scheduler -> IO ()
-destroy (Scheduler nm rconn mv _) =
+destroy (Scheduler (Name nm) rconn mv _ _) =
   do tasks <- readMVar mv
      R.runRedis rconn $
        R.del (concatMap (\(tnm,_,_) ->
-                            [lastRunKey nm tnm
+                            [lastStartedKey nm tnm
                             ,lockedKey nm tnm])
                         tasks)
      return ()
-
-nextRunTime :: Period -> UTCTime -> Maybe UTCTime -> UTCTime
-nextRunTime (Daily (Time t)) now _ = now { utctDayTime = t }
-nextRunTime (Every (Seconds n)) now Nothing = now
-nextRunTime (Every (Seconds n)) now (Just last) =
-  now { utctDayTime = utctDayTime last + fromIntegral n}
 
 collapseError :: Either a (Maybe b) -> Maybe b
 collapseError (Left _) = Nothing
@@ -75,55 +79,52 @@ parseUnixTime = posixSecondsToUTCTime . fromIntegral . read . T.unpack . T.decod
 
 renderUnixTime = T.encodeUtf8 . T.pack . show . (round :: NominalDiffTime -> Integer) . utcTimeToPOSIXSeconds
 
-close :: NominalDiffTime -> UTCTime -> UTCTime -> Bool
-close interval a b = abs (diffUTCTime a b) < interval
-
-further :: NominalDiffTime -> UTCTime -> UTCTime -> Bool
-further interval a b = not (close interval a b)
+minutes5 = 60 * 5
 
 shouldRun :: Period -> Maybe UTCTime -> Maybe UTCTime -> UTCTime -> Bool
-shouldRun (Daily (Time t)) Nothing locked now
-  | now > (now { utctDayTime = t }) && diffUTCTime now (now { utctDayTime = t }) < 60
+shouldRun _ _ (Just locked) now
+  = diffUTCTime now locked > lockTimeout
+shouldRun (Daily (Time t)) Nothing _ now
+  | now > (now { utctDayTime = t }) && utctDayTime now - t < minutes5
   = True
-shouldRun (Daily (Time t)) (Just last) locked now
-  | now > (now { utctDayTime = t })
+shouldRun (Daily (Time t)) (Just last) _ now
+  | now > (now { utctDayTime = t }) && last < (now { utctDayTime = t })
   = True
-shouldRun period last locked no = False
+shouldRun (Every (Seconds n)) Nothing _ now
+  = True
+shouldRun (Every (Seconds n)) (Just last) locked now
+  = diffUTCTime now last > fromIntegral n
+shouldRun period last locked now = False
+
 
 tryRunTask :: Int -> Text -> R.Connection -> UTCTime -> (Text, Period, IO ()) -> IO ()
 tryRunTask check pname rconn now (name, period, task) =
-  do lastRun <- fmap parseUnixTime . collapseError <$>
-                R.runRedis rconn
-                           (R.get (lastRunKey pname name))
+  do lastStarted <- fmap parseUnixTime . collapseError <$>
+                    R.runRedis rconn
+                               (R.get (lastStartedKey pname name))
      lockedAt <- fmap parseUnixTime . collapseError <$>
                  R.runRedis rconn
                             (R.get (lockedKey pname name))
-     let nextRun = nextRunTime period now lastRun
-     print (lastRun, now, nextRun, shouldRun period lastRun lockedAt now)
-     when (shouldRun period lastRun lockedAt now) $
+     when (shouldRun period lastStarted lockedAt now) $
        do gotLock <-
             collapseNumberBoolFalse <$> R.runRedis rconn
             (R.eval "local lock = redis.call('get', KEYS[1])\n\
-                    \if (lock == false and ARGV[1] == '0') or (lock == ARGV[1]) then\n\
-                    \  redis.call('set', KEYS[1], ARGV[2])\n\
+                    \local last = redis.call('get', KEYS[2])\n\
+                    \if ((lock == false and ARGV[1] == '0') or (lock == ARGV[1]))\
+                    \  and ((last == false and ARGV[2] == '0') or (last == ARGV[2])) then\n\
+                    \  redis.call('set', KEYS[1], ARGV[3])\n\
+                    \  redis.call('set', KEYS[2], ARGV[3])\n\
                     \  return 1\n\
                     \else\n\
                     \  return 0\n\
-                    \end" [lockedKey pname name]
+                    \end" [lockedKey pname name, lastStartedKey pname name]
                           [maybe "0" renderUnixTime lockedAt
-                          , renderUnixTime now])
-          print gotLock
+                          ,maybe "0" renderUnixTime lastStarted
+                          ,renderUnixTime now])
           when gotLock $
             do task
                -- TODO(dbp 2016-05-26): Run task in
                -- thread to handle failure, log it
                -- somehow...
-               r <- R.runRedis rconn
-                               (R.eval "redis.call('del', KEYS[1])\n\
-                                       \redis.call('set', KEYS[2], ARGV[1])"
-                                       [ lockedKey pname name
-                                       , lastRunKey pname name]
-                                       [renderUnixTime now])
-                 :: IO (Either R.Reply (Maybe Integer))
-               print r
+               R.runRedis rconn $ R.del [lockedKey pname name]
                return ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -22,21 +22,20 @@ time t h m s = t { utctDayTime = fromIntegral $ h * 60 * 60 + m * 60 + s }
 main :: IO ()
 main = hspec $
   do describe "shouldRun" $
-       do it "should run if it's not locked and next time is near now" $
-            shouldRun 100 Nothing (date 2016 5 5) (time (date 2016 5 5) 0 0 10)
+       do it "should run if it's never been run, not locked and time just passed" $
+            shouldRun (Daily (Time 5)) Nothing Nothing (time (date 2016 5 5) 0 0 6)
           it "shouldn't run if it was locked recently" $
-            not $ shouldRun 100 (Just (date 2016 5 5))
-                                (time (date 2016 5 5) 0 3 0)
-                                (time (date 2016 5 5) 0 0 10)
+            not $ shouldRun (Daily (Time 5)) (Just (time (date 2016 5 4) 23 45 0)) (Just (time (date 2016 5 4) 23 45 0)) (time (date 2016 5 5) 0 0 6)
           it "should run if it was locked a long time ago" $
-            shouldRun 100 (Just (date 2016 5 4))
-                          (date 2016 5 5)
-                          (time (date 2016 5 5) 0 0 10)
+            shouldRun (Daily (Time 5))
+                      (Just (date 2016 5 4))
+                      (Just (date 2016 5 4))
+                      (date 2016 5 5)
      describe "simple job" $
        do it "should run " $
             do mvar <- newMVar 0
                rconn <- R.connect R.defaultConnectInfo
-               scheduler <- create "simple-1" rconn 1
+               scheduler <- create (Name "simple-1") rconn (CheckInterval (Seconds 1)) (LockTimeout (Seconds 1000))
                addTask scheduler "job-1" (Every (Seconds 100)) (modifyMVarMasked_ mvar (return . (+1)))
 
                wthread <- forkIO (run scheduler)
@@ -48,7 +47,7 @@ main = hspec $
           it "should only run once per scheduled time" $
              do mvar <- newMVar 0
                 rconn <- R.connect R.defaultConnectInfo
-                scheduler <- create "simple-2" rconn 1
+                scheduler <- create (Name "simple-2") rconn (CheckInterval (Seconds 1)) (LockTimeout (Seconds 1000))
                 addTask scheduler "job-2" (Every (Seconds 100)) (modifyMVarMasked_ mvar (return . (+1)))
                 wthread1 <- forkIO (run scheduler)
                 wthread2 <- forkIO (run scheduler)
@@ -63,57 +62,15 @@ main = hspec $
           it "should run at each time point" $
              do mvar <- newMVar 0
                 rconn <- R.connect R.defaultConnectInfo
-                scheduler <- create "simple-3" rconn 1
-                addTask scheduler "job-3" (Every (Seconds 3)) (modifyMVarMasked_ mvar (return . (+1)))
+                scheduler <- create (Name "simple-3") rconn (CheckInterval (Seconds 1)) (LockTimeout (Seconds 1000))
+                addTask scheduler "job-3" (Every (Seconds 1)) (modifyMVarMasked_ mvar (return . (+1)))
                 wthread1 <- forkIO (run scheduler)
                 wthread2 <- forkIO (run scheduler)
                 wthread3 <- forkIO (run scheduler)
-                threadDelay 10000000
+                threadDelay 2900000
                 killThread wthread1
                 killThread wthread2
                 killThread wthread3
                 destroy scheduler
                 v <- takeMVar mvar
                 v `shouldBe` 3
-     --      it "queueing 2 jobs should increment twice" $
-     --        do mvar <- newMVar 0
-     --           hworker <- createWith (conf "simpleworker-2"
-     --                                       (SimpleState mvar))
-     --           wthread <- forkIO (worker hworker)
-     --           queue hworker SimpleJob
-     --           queue hworker SimpleJob
-     --           threadDelay 40000
-     --           killThread wthread
-     --           destroy hworker
-     --           v <- takeMVar mvar
-     --           assertEqual "State should be 2 after 2 jobs run" 2 v
-     --      it "queueing 1000 jobs should increment 1000" $
-     --        do mvar <- newMVar 0
-     --           hworker <- createWith (conf "simpleworker-3"
-     --                                       (SimpleState mvar))
-     --           wthread <- forkIO (worker hworker)
-     --           replicateM_ 1000 (queue hworker SimpleJob)
-     --           threadDelay 2000000
-     --           killThread wthread
-     --           destroy hworker
-     --           v <- takeMVar mvar
-     --           assertEqual "State should be 1000 after 1000 job runs" 1000 v
-     --      it "should work with multiple workers" $
-     --      -- NOTE(dbp 2015-07-12): This probably won't run faster, because
-     --      -- they are all blocking on the MVar, but that's not the point.
-     --        do mvar <- newMVar 0
-     --           hworker <- createWith (conf "simpleworker-4"
-     --                                       (SimpleState mvar))
-     --           wthread1 <- forkIO (worker hworker)
-     --           wthread2 <- forkIO (worker hworker)
-     --           wthread3 <- forkIO (worker hworker)
-     --           wthread4 <- forkIO (worker hworker)
-     --           replicateM_ 1000 (queue hworker SimpleJob)
-     --           threadDelay 1000000
-     --           killThread wthread1
-     --           killThread wthread2
-     --           killThread wthread3
-     --           killThread wthread4
-     --           destroy hworker
-     --           v <- takeMVar mvar
-     --           assertEqual "State should be 1000 after 1000 job runs" 1000 v
